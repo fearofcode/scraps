55e6908713d87c86164bfd25f005a013271b8e9d
Warren Henning <warren.henning@gmail.com>
2010-09-13 16:42:13 -0700

    Video ranking added

--- videorank.py
+++ videorank.py
@@ -2,18 +2,35 @@ import urllib
 import xml.dom.minidom
 import logging
 import sys
+from datetime import datetime
 
 class Video(object):
     def __init__(self, title, published, viewCount, url):
         self.title = title
-        self.published = published
+
+        if type(published) == type(u"") or type(published) == type(""):
+            self.published = datetime.strptime(published, "%Y-%m-%dT%H:%M:%S.000Z") # assume iso 8601 format
+        else:
+            self.published = published
+
         self.viewCount = viewCount
         self.url = url
 
     def __repr__(self):
         return "<Video object %s>" % dict(title=self.title, published=self.published, 
-                                          viewCount=self.viewCount, url=self.url)
+                                          viewCount=self.viewCount, url=self.url, rank=self.rank())
 
+    def rank(self):
+        "Compute how hot a video is. Borrowed from Hacker News' ranking formula."
+        daysOld = (datetime.today()-self.published).days
+
+        return (self.viewCount - 1.)/(daysOld+2.)**1.5
+
+    @classmethod
+    def rankVideos(self, videoList):
+        return sorted(videoList, key=lambda video: video.rank(), reverse=True)
+
+class VideoFeedRetriever(object):
     @classmethod
     def fetchAndParseFeed(self, url):
         logging.debug("Attempting to retrieve %s" % url)
@@ -32,16 +49,13 @@ class Video(object):
         videos = []
 
         for entry in entries:
-            try:
-                title = entry.getElementsByTagName("title")[0].childNodes[0].data
-                published = entry.getElementsByTagName("published")[0].childNodes[0].data
-                viewCount = int(entry.getElementsByTagName("yt:statistics")[0].getAttribute("viewCount"))
-                url = [elem for elem in entry.getElementsByTagName("link") 
-                       if elem.hasAttribute("rel") and elem.getAttribute("rel") == "alternate"][0].getAttribute("href")
-                videos.append(Video(title, published, viewCount, url))
-            except:
-                logging.error("Something went wrong with the XML feed.")
-                sys.exit(-1)
+            title = entry.getElementsByTagName("title")[0].childNodes[0].data
+            published = entry.getElementsByTagName("published")[0].childNodes[0].data
+            viewCount = int(entry.getElementsByTagName("yt:statistics")[0].getAttribute("viewCount"))
+            url = [elem for elem in entry.getElementsByTagName("link") 
+                   if elem.hasAttribute("rel") and elem.getAttribute("rel") == "alternate"][0].getAttribute("href")
+            videos.append(Video(title, published, viewCount, url))
+
 
         return videos
 
@@ -71,7 +85,11 @@ class Video(object):
 
         return videos
 
+    @classmethod
+    def getRankedEntriesFromChannel(self, username):
+        return Video.rankVideos(self.getAllEntriesFromChannel(username))
+
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
 
-    videos = Video.getAllEntriesFromChannel("haveadot")
+    videos = VideoFeedRetriever.getRankedEntriesFromChannel("thunderdome94")
